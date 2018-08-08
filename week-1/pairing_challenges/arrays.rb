e6cff719b89b81bea4d2053aac8b79d6d3e93553
Alastair Edmonds <alastair.edmonds@gmail.com>
2018-08-08 07:47:13 +0100

    splitting out exercises to separate files

--- week-1/pairing_challenges/arrays.rb
+++ week-1/pairing_challenges/arrays.rb
@@ -1,3 +1,7 @@
+### Challenge Set 1: Arrays
+
+Transform the following first arrays into the second entities:
+
 arr1 = [1, 2, 3, 4, 5] #=> [2, 3, 4, 5, 6]
 arr2 = [1, 3, 5, 4, 2] #=> [1, 2, 3, 4, 5]
 arr3 = [1, 3, 5, 4, 2] #=> [2, 3, 4, 5, 6]
@@ -23,28 +27,3 @@ end
 def array_fifth(array)
   2 * array_fourth(array)
 end
-
-h1 = { a: 1, b: 2 } #=> { a: 2, b: 3 }
-h2 = { a: 2, b: 5, c: 1 } #=> { a: 1, b: 2, c: 5 }
-h3 = { a: 2, b: 5, c: 1 } #=> [1, 2, 5]
-h4 = { a: 1, b: 2, c: 3 } #=> { "1"=>"a", "2"=>"b", "3"=>"c" }
-
-def hash_first(hash)
-  hash.transform_values { |v| v + 1 }
-end
-
-def hash_second(hash)
-  keys = hash.keys
-  values = hash.values
-  values.unshift(values.pop)
-  keys.zip(values.first(values.length)).to_h
-end
-
-def hash_third(hash)
-  hash.values.sort
-end
-
-def hash_fourth(hash)
-  hash.values.zip(hash.keys).to_h
-end
-
